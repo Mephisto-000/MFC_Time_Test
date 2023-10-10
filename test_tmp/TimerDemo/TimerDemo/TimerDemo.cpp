bdd2211f868fb2185aee91a1dcff99c911bbd8b7
Mephisto Lin <Mephisto007linlin@outlook.com>
2023-10-11 07:34:40 +0800

    feat: update

---
+++ test_tmp/TimerDemo/TimerDemo/TimerDemo.cpp
@@ -0,0 +1,77 @@
+﻿#include <windows.h>
+#include <iostream>
+
+// 定義計時器的ID，用於識別是哪一個計時器所觸發的事件
+#define MY_TIMER_ID 1
+
+LRESULT CALLBACK TestProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+{
+    switch (uMsg) 
+    {
+    case WM_CREATE: 
+    {   // 當窗口被創建時，我們設定一個計時器，每1000毫秒（即1秒）就觸發一次
+        
+        SetTimer(hwnd, MY_TIMER_ID, 1000, NULL);
+    } break;
+
+    case WM_TIMER: 
+    {   // 當計時器觸發時，會收到此消息
+        // 我們檢查是哪一個計時器，然後執行相對應的動作
+        
+        if (wParam == MY_TIMER_ID) {
+            std::cout << "Timer ticked!" << std::endl;  // 輸出訊息表示計時器已觸發
+        }
+    } break;
+
+    case WM_DESTROY: 
+    {   // 窗口被銷毀前，銷毀我們之前創建的計時器，釋放資源
+        
+        KillTimer(hwnd, MY_TIMER_ID);
+        PostQuitMessage(0);  // 發送結束消息，結束消息循環
+        return 0;
+    } break;
+
+    default:
+        // 其他消息，交由默認的窗口處理函數處理
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
+int main() {
+    // 註冊窗口類，為了創建窗口
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = TestProc;  // 指定窗口消息處理函數
+    wc.hInstance = GetModuleHandle(NULL);  // 獲取當前應用程式的實例句柄
+    wc.lpszClassName = L"SampleTimerWindow";  // 指定窗口類的名稱
+
+    RegisterClass(&wc);  // 註冊窗口類
+
+    // 創建窗口
+    HWND hwnd = CreateWindowEx(
+        0,                              // 額外的窗口風格
+        L"SampleTimerWindow",           // 窗口類名稱
+        L"SetTimer Example",            // 窗口的標題
+        WS_OVERLAPPEDWINDOW,            // 基本的窗口風格
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,       // 父窗口的句柄
+        NULL,       // 菜單的句柄
+        GetModuleHandle(NULL),  // 應用程式的實例句柄
+        NULL        // 額外的應用程式數據
+    );
+
+    if (hwnd == NULL) {
+        return 0;  // 若窗口創建失敗，則結束程式
+    }
+
+    ShowWindow(hwnd, SW_SHOW);  // 顯示窗口
+
+    // 消息循環，處理並分發窗口消息
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);  // 進行消息翻譯
+        DispatchMessage(&msg);   // 分發消息至對應的窗口處理函數
+    }
+
+    return 0;  // 程式結束
+}
---
+++ test_tmp/WaitableTimerDemo/WaitableTimerDemo/WaitableTimerDemo.cpp
@@ -0,0 +1,62 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <thread>
+
+// 這個執行緒將等待計時器
+void TimerThread(HANDLE hTimer) 
+{   // 等待計時器信號，這將使執行緒暫停，直到計時器被觸發
+    
+    if (WaitForSingleObject(hTimer, INFINITE) != WAIT_OBJECT_0) {
+        std::cerr << "WaitForSingleObject failed!" << std::endl;  // 等待失敗時輸出錯誤信息
+    }
+    else {
+        std::cout << "Thread 1: Timer was signaled!" << std::endl;  // 計時器被觸發時，輸出提示信息
+    }
+}
+
+// 這是另一個將等待計時器的執行緒
+void AnotherTimerThread(HANDLE hTimer) 
+{   // 與上面的執行緒功能相同，只是用於展示多個執行緒可以等待同一個計時器
+
+    if (WaitForSingleObject(hTimer, INFINITE) != WAIT_OBJECT_0) {
+        std::cerr << "WaitForSingleObject failed!" << std::endl;
+    }
+    else {
+        std::cout << "Thread 2: Timer was signaled!" << std::endl;
+    }
+}
+
+int main() {
+    HANDLE hTimer = NULL;  // 定義一個計時器控制代碼
+    LARGE_INTEGER liDueTime;  // 用於設置計時器的時間
+
+    liDueTime.QuadPart = -100000000LL; // 設置計時器為10秒。負值表示相對時間。
+
+    // 創建一個未命名的可等待計時器
+    hTimer = CreateWaitableTimer(NULL, TRUE, NULL);
+    if (NULL == hTimer) {
+        std::cerr << "CreateWaitableTimer failed!" << std::endl;  // 創建計時器失敗時輸出錯誤信息
+        return 1;
+    }
+
+    // 啟動兩個執行緒，它們都將等待上面創建的計時器
+    std::thread t1(TimerThread, hTimer);  // 第一個執行緒
+    std::thread t2(AnotherTimerThread, hTimer);  // 第二個執行緒
+
+    std::cout << "Main thread: Waiting for 10 seconds..." << std::endl;  // 主執行緒輸出等待信息
+
+    // 啟動計時器。當計時器過了10秒後，它將發出信號。
+    if (!SetWaitableTimer(hTimer, &liDueTime, 0, NULL, NULL, 0)) {
+        std::cerr << "SetWaitableTimer failed!" << std::endl;  // 啟動計時器失敗時輸出錯誤信息
+        return 2;
+    }
+
+    // 主執行緒將等待上面創建的兩個執行緒完成它們的任務
+    t1.join();  // 等待第一個執行緒完成
+    t2.join();  // 等待第二個執行緒完成
+
+    CloseHandle(hTimer);  // 關閉計時器控制代碼
+
+    return 0;  // 主執行緒結束
+}
+
